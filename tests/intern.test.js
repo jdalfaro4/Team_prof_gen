688a193f1efb3644fc3cd5b2c98dfcadff6819c9
jdalfaro4 <jdalfaro4@gmail.com>
2023-02-27 21:32:32 -0600

    updated intern test

---
+++ tests/intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require('../lib/Intern')
+
+describe('Intern class test', () => {
+    describe('init', () => {
+        it('test constructor properties', ()=> {
+            let intObj = new Intern('John', 'jdalfaro4@getMaxListeners.com', 1, "UT");
+
+            expect(intObj.name).toEqual('John')
+            expect(intObj.email).toEqual('jdalfaro4@gmail.com');
+            expect(intObj.id).toEqual(1);
+            expect(intObj.role).toEqual('Intern');
+            expect(intObj.school).toEqual('UT');
+
+        })
+    })
+
+    describe('test get school function', () => {
+        let intObj = new Intern("max", "max12@ymail.com", 2, "UT");
+
+        expect(intObj.getSchool()).toEqual("UT")
+    })
+})
