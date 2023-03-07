5fdf8e78d99b798a58e334e93aeefd59accc462e
jdalfaro4 <jdalfaro4@gmail.com>
2023-03-06 21:05:30 -0600

    created css to make employee boxes more appealing

--- generatehtml.js
+++ generatehtml.js
@@ -27,34 +27,66 @@ let generateHTML = function(data) {
 
 function generateEngineerHtml(engineer){
     return `
-    <div id="Engineer">
-    <p> Engineer Name - ${engineer.getName()} </p>
-    <p> Engineer Email - ${engineer.getEmail()} </p>
-    <p> Engineer Id - ${engineer.getId()} </p>
-    <p> Engineer Github - ${engineer.getGithub()} </p>
-
+    <div id="employee-container">
+    <div id="employee-header">
+      <h2>Engineer</h2>
+      <h3>${engineer.getName()} </h3>
+    </div>
+    <div class="info-container">
+      <span class="info-label">Email:</span>
+      <span class="info-value">${engineer.getEmail()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">ID:</span>
+      <span class="info-value">${engineer.getId()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">Github::</span>
+      <span class="info-value">${engineer.getGithub()}</span>
     </div>
     `
 }
 
 function generateManagerHtml(manager){
     return `
-    <div id="Manager">
-    <p> Manager Name - ${manager.getName()} </p>
-    <p> Manager Email - ${manager.getEmail()} </p>
-    <p> Manager Id - ${manager.getId()} </p>
-    <p> Manager Officenumber - ${manager.getOfficeNumber()} </p>
+    <div id="employee-container">
+    <div id="employee-header">
+      <h2>Manager</h2>
+      <h3>${manager.getName()}</h3>
+    </div>
+    <div class="info-container">
+      <span class="info-label">Email:</span>
+      <span class="info-value">${manager.getEmail()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">ID:</span>
+      <span class="info-value">${manager.getId()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">Office Number:</span>
+      <span class="info-value">${manager.getOfficeNumber()}</span>
     </div>
     `
 }
 
 function generateInterHtml(intern){
     return `
-    <div id="Intern">
-    <p> Intern Name - ${intern.getName()} </p>
-    <p> Intern Email - ${intern.getEmail()} </p>
-    <p> Intern Id - ${intern.getId()} </p>
-    <p> Intern School - ${intern.getSchool()} </p>
+    <div id="employee-container">
+    <div id="employee-header">
+      <h2>Intern</h2>
+      <h3>${intern.getName()} </h3>
+    </div>
+    <div class="info-container">
+      <span class="info-label">Email:</span>
+      <span class="info-value">${intern.getEmail()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">ID:</span>
+      <span class="info-value">${intern.getId()}</span>
+    </div>
+    <div class="info-container">
+      <span class="info-label">School:</span>
+      <span class="info-value">${intern.getSchool()}</span>
     </div>
     `
 }
